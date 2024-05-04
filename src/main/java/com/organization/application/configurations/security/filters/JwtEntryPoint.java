c082c0f74b1bc78fb27db025493c4e5b69106b25
OscarRuina <oscarruina@hotmail.com>
2024-05-04 12:53:37 -0300

    remove imports

--- src/main/java/com/organization/application/configurations/security/filters/JwtEntryPoint.java
+++ src/main/java/com/organization/application/configurations/security/filters/JwtEntryPoint.java
@@ -2,12 +2,9 @@ package com.organization.application.configurations.security.filters;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.organization.application.dtos.response.ApplicationResponse;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
@@ -20,7 +17,7 @@ public class JwtEntryPoint implements AuthenticationEntryPoint {
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
-            AuthenticationException authException) throws IOException, ServletException {
+            AuthenticationException authException) throws IOException {
 
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
