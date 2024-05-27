d8b551aa7b7984983f15a685ce7c74f20c9a2886
Oscar Ruina <31217980+OscarRuina@users.noreply.github.com>
2024-05-27 14:03:54 -0300

    Feature MimeMesaage Email (#1)
    * refactor mail service
    
    * minor fixs

--- src/main/java/com/organization/application/configurations/email/service/IEmailService.java
+++ src/main/java/com/organization/application/configurations/email/service/IEmailService.java
@@ -1,6 +1,8 @@
 package com.organization.application.configurations.email.service;
 
+import java.util.Map;
+
 public interface IEmailService {
 
-    void sendEmail(String[] toUser, String subject, String message);
+    void sendEmail(String[] toUser, String subject, Map<String, Object> message);
 }
